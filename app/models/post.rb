a63ffa8936f23a9a6a171708b084c94bd6cb032b
Raghu Betina <raghu@firstdraft.com>
2018-01-08 00:20:31 +0000

    Direct association between posts and posts_likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ApplicationRecord
   # Direct associations
 
+  has_many   :likes,
+             :class_name => "PostsLike",
+             :dependent => :destroy
+
   has_many   :comments,
              :dependent => :destroy
 
--- app/models/posts_like.rb
+++ app/models/posts_like.rb
@@ -1,6 +1,8 @@
 class PostsLike < ApplicationRecord
   # Direct associations
 
+  belongs_to :post
+
   belongs_to :user
 
   # Indirect associations
