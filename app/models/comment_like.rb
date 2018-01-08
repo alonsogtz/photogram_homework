16efba19ad575b5e892b79f9d84efffc4b3bf515
Raghu Betina <raghu@firstdraft.com>
2018-01-08 00:20:31 +0000

    Direct association between users and comment_likes

--- app/models/comment_like.rb
+++ app/models/comment_like.rb
@@ -1,6 +1,8 @@
 class CommentLike < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :comment_likes,
+             :dependent => :destroy
+
   has_many   :likes,
              :class_name => "PostsLike",
              :dependent => :destroy
